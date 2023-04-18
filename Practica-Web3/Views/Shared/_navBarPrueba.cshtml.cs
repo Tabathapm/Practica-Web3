6826672a5bf2fa732552fed408425b8ea6828234
Tabathapm <tabathapm@gmail.com>
2023-04-18 20:02:29 -0300

    implementacion de navbar personalizado

---
+++ Practica-Web3/Views/Shared/_navBarPrueba.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica_Web3.Views.Shared
+{
+    public class navBarPruebaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
